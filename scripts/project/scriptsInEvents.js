0de7d622214724120fb7cb891558291f20ae9994
Dev33IO <127096734+Dev33IO@users.noreply.github.com>
2023-06-07 19:36:43 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,27 @@
+
+
+
+const scriptsInEvents = {
+
+	async Egame_Event9(runtime, localVars)
+	{
+		// Отримуємо доступ до SVG елемента за його ID
+		var svgElement = document.getElementById("et");
+		
+		// Отримуємо всі елементи <path> всередині SVG
+		var pathElements = svgElement.querySelectorAll("path");
+		
+		// Додаємо обробник події click до кожного елемента <path>
+		pathElements.forEach(function(pathElement) {
+		  pathElement.addEventListener("click", function(event) {
+		    // Код, який виконується при кліку на <path>
+		    console.log("Клікнуто на <path>");
+		  });
+		});
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
